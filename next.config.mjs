c9f65ece837e7993104efb480adbb9721b2a8d3b
Mino-rex <macminosp@icloud.com>
2025-03-10 01:21:46 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,12 @@
 import path from 'path';
 
 /** @type {import('next').NextConfig} */
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ['6lo2x1gvpznrimav.public.blob.vercel-storage.com'],
