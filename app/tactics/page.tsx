90885d1df6aa00257f3b277ca57bf586174e6099
Mino-rex <macminosp@icloud.com>
2025-04-18 15:21:54 +0900

    Modify player positions of each formation

--- app/tactics/page.tsx
+++ app/tactics/page.tsx
@@ -22,78 +22,78 @@ type Player = {
 // 4-4-2 formation for home team
 const formation442Home: Omit<Player, "id">[] = [
   // Goalkeeper
-  { team: "home", position: { x: 10, y: 50 }, number: 1, positionName: "GK", direction: 0 },
+  { team: "home", position: { x: 5, y: 50 }, number: 1, positionName: "GK", direction: 0 },
   // Defenders (4)
   { team: "home", position: { x: 20, y: 20 }, number: 2, positionName: "RSB", direction: 0 },
   { team: "home", position: { x: 20, y: 40 }, number: 3, positionName: "CB", direction: 0 },
   { team: "home", position: { x: 20, y: 60 }, number: 4, positionName: "CB", direction: 0 },
   { team: "home", position: { x: 20, y: 80 }, number: 5, positionName: "LSB", direction: 0 },
   // Midfielders (4)
-  { team: "home", position: { x: 40, y: 20 }, number: 6, positionName: "RMF", direction: 0 },
-  { team: "home", position: { x: 40, y: 40 }, number: 7, positionName: "CMF", direction: 0 },
-  { team: "home", position: { x: 40, y: 60 }, number: 8, positionName: "CMF", direction: 0 },
-  { team: "home", position: { x: 40, y: 80 }, number: 9, positionName: "LMF", direction: 0 },
+  { team: "home", position: { x: 33, y: 20 }, number: 6, positionName: "RMF", direction: 0 },
+  { team: "home", position: { x: 33, y: 40 }, number: 7, positionName: "CMF", direction: 0 },
+  { team: "home", position: { x: 33, y: 60 }, number: 8, positionName: "CMF", direction: 0 },
+  { team: "home", position: { x: 33, y: 80 }, number: 9, positionName: "LMF", direction: 0 },
   // Forwards (2)
-  { team: "home", position: { x: 60, y: 35 }, number: 10, positionName: "CF", direction: 0 },
-  { team: "home", position: { x: 60, y: 65 }, number: 11, positionName: "CF", direction: 0 },
+  { team: "home", position: { x: 46, y: 40 }, number: 10, positionName: "CF", direction: 0 },
+  { team: "home", position: { x: 46, y: 60 }, number: 11, positionName: "CF", direction: 0 },
 ]
 
 // 4-3-3 formation for home team
 const formation433Home: Omit<Player, "id">[] = [
   // Goalkeeper
-  { team: "home", position: { x: 10, y: 50 }, number: 1, positionName: "GK", direction: 0 },
+  { team: "home", position: { x: 5, y: 50 }, number: 1, positionName: "GK", direction: 0 },
   // Defenders (4)
   { team: "home", position: { x: 20, y: 20 }, number: 2, positionName: "RSB", direction: 0 },
   { team: "home", position: { x: 20, y: 40 }, number: 3, positionName: "CB", direction: 0 },
   { team: "home", position: { x: 20, y: 60 }, number: 4, positionName: "CB", direction: 0 },
   { team: "home", position: { x: 20, y: 80 }, number: 5, positionName: "LSB", direction: 0 },
   // Midfielders (3)
-  { team: "home", position: { x: 40, y: 30 }, number: 6, positionName: "CMF", direction: 0 },
-  { team: "home", position: { x: 40, y: 50 }, number: 7, positionName: "CMF", direction: 0 },
-  { team: "home", position: { x: 40, y: 70 }, number: 8, positionName: "CMF", direction: 0 },
+  { team: "home", position: { x: 30, y: 35 }, number: 6, positionName: "CMF", direction: 0 },
+  { team: "home", position: { x: 30, y: 50 }, number: 7, positionName: "CMF", direction: 0 },
+  { team: "home", position: { x: 30, y: 65 }, number: 8, positionName: "CMF", direction: 0 },
   // Forwards (3)
-  { team: "home", position: { x: 50, y: 20 }, number: 9, positionName: "LWG", direction: 0 },
-  { team: "home", position: { x: 60, y: 50 }, number: 10, positionName: "CF", direction: 0 },
-  { team: "home", position: { x: 50, y: 80 }, number: 11, positionName: "RWG", direction: 0 },
+  { team: "home", position: { x: 40, y: 20 }, number: 9, positionName: "LWG", direction: 0 },
+  { team: "home", position: { x: 47, y: 50 }, number: 10, positionName: "CF", direction: 0 },
+  { team: "home", position: { x: 40, y: 80 }, number: 11, positionName: "RWG", direction: 0 },
 ]
 
 // 4-2-3-1 formation for home team
 const formation4231Home: Omit<Player, "id">[] = [
   // Goalkeeper
-  { team: "home", position: { x: 10, y: 50 }, number: 1, positionName: "GK", direction: 0 },
+  { team: "home", position: { x: 5, y: 50 }, number: 1, positionName: "GK", direction: 0 },
   // Defenders (4)
   { team: "home", position: { x: 20, y: 20 }, number: 2, positionName: "RSB", direction: 0 },
   { team: "home", position: { x: 20, y: 40 }, number: 3, positionName: "CB", direction: 0 },
   { team: "home", position: { x: 20, y: 60 }, number: 4, positionName: "CB", direction: 0 },
   { team: "home", position: { x: 20, y: 80 }, number: 5, positionName: "LSB", direction: 0 },
   // Defensive Midfielders (2)
-  { team: "home", position: { x: 35, y: 40 }, number: 6, positionName: "DMF", direction: 0 },
-  { team: "home", position: { x: 35, y: 60 }, number: 7, positionName: "DMF", direction: 0 },
+  { team: "home", position: { x: 30, y: 40 }, number: 6, positionName: "DMF", direction: 0 },
+  { team: "home", position: { x: 30, y: 60 }, number: 7, positionName: "DMF", direction: 0 },
   // Attacking Midfielders (3)
-  { team: "home", position: { x: 50, y: 20 }, number: 8, positionName: "LMF", direction: 0 },
-  { team: "home", position: { x: 50, y: 50 }, number: 9, positionName: "OMF", direction: 0 },
-  { team: "home", position: { x: 50, y: 80 }, number: 10, positionName: "RMF", direction: 0 },
+  { team: "home", position: { x: 38, y: 20 }, number: 8, positionName: "LMF", direction: 0 },
+  { team: "home", position: { x: 38, y: 50 }, number: 9, positionName: "OMF", direction: 0 },
+  { team: "home", position: { x: 38, y: 80 }, number: 10, positionName: "RMF", direction: 0 },
   // Forward (1)
-  { team: "home", position: { x: 65, y: 50 }, number: 11, positionName: "CF", direction: 0 },
+  { team: "home", position: { x: 47, y: 50 }, number: 11, positionName: "CF", direction: 0 },
 ]
 
 // 4-4-2 formation for away team
 const formation442Away: Omit<Player, "id">[] = [
   // Goalkeeper
-  { team: "away", position: { x: 90, y: 50 }, number: 1, positionName: "GK", direction: 180 },
+  { team: "away", position: { x: 95, y: 50 }, number: 1, positionName: "GK", direction: 180 },
   // Defenders (4)
   { team: "away", position: { x: 80, y: 20 }, number: 2, positionName: "RSB", direction: 180 },
   { team: "away", position: { x: 80, y: 40 }, number: 3, positionName: "CB", direction: 180 },
   { team: "away", position: { x: 80, y: 60 }, number: 4, positionName: "CB", direction: 180 },
   { team: "away", position: { x: 80, y: 80 }, number: 5, positionName: "LSB", direction: 180 },
   // Midfielders (4)
-  { team: "away", position: { x: 60, y: 20 }, number: 6, positionName: "RMF", direction: 180 },
-  { team: "away", position: { x: 60, y: 40 }, number: 7, positionName: "CMF", direction: 180 },
-  { team: "away", position: { x: 60, y: 60 }, number: 8, positionName: "CMF", direction: 180 },
-  { team: "away", position: { x: 60, y: 80 }, number: 9, positionName: "LMF", direction: 180 },
+  { team: "away", position: { x: 67, y: 20 }, number: 6, positionName: "RMF", direction: 180 },
+  { team: "away", position: { x: 67, y: 40 }, number: 7, positionName: "CMF", direction: 180 },
+  { team: "away", position: { x: 67, y: 60 }, number: 8, positionName: "CMF", direction: 180 },
+  { team: "away", position: { x: 67, y: 80 }, number: 9, positionName: "LMF", direction: 180 },
   // Forwards (2)
-  { team: "away", position: { x: 40, y: 35 }, number: 10, positionName: "CF", direction: 180 },
-  { team: "away", position: { x: 40, y: 65 }, number: 11, positionName: "CF", direction: 180 },
+  { team: "away", position: { x: 54, y: 40 }, number: 10, positionName: "CF", direction: 180 },
+  { team: "away", position: { x: 54, y: 60 }, number: 11, positionName: "CF", direction: 180 },
 ]
 
 // Create initial players with IDs
